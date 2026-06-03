18834f6c5fed870e50169ce4be8413122b9969d6
Mathieu Réquillart <mathieu.requillart@gmail.com>
2026-06-04 01:17:45 +0200

    bluetooth(output): add odio-api v0.14.0 docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -60,6 +60,7 @@ export default defineConfig({
 					label: 'Features',
 					items: [
 						{ label: 'Bluetooth', slug: 'guides/bluetooth' },
+						{ label: 'Bluetooth output', slug: 'guides/bluetooth-output' },
 						{ label: 'AirPlay', slug: 'guides/airplay' },
 						{ label: 'Spotify Connect', slug: 'guides/spotify' },
 						{ label: 'UPnP / DLNA', slug: 'guides/dlna' },
@@ -90,7 +91,6 @@ export default defineConfig({
 						{ label: 'HTPC', slug: 'guides/use-case-htpc' },
 						{ label: 'NAS', slug: 'guides/use-case-nas' },
 						{ label: 'Phone & Tablet', slug: 'guides/use-case-phone' },
-						{ label: 'Bluetooth output', slug: 'guides/bluetooth-output' },
 						{ label: 'Navidrome', slug: 'guides/navidrome' },
 						{ label: 'Music Assistant', slug: 'guides/music-assistant' },
 					],
